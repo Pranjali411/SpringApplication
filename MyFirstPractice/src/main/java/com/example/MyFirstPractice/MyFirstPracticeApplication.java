b88fa7a63d915bc01baca6b9dcbdb11ad436b32d
Pranjali <Pranjalimote@gmail.com>
2021-06-01 20:55:45 +0530

    Project Created

---
+++ MyFirstPractice/src/main/java/com/example/MyFirstPractice/MyFirstPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstPracticeApplication.class, args);
+	}
+
+}
---
+++ MyFirstPractice/src/test/java/com/example/MyFirstPractice/MyFirstPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
